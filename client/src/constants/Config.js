65a760d64040cefd3ade22b01358656d00ccd536
duongdh1 <duongdh@vmogroup.com>
2023-10-12 17:47:35 +0700

    update

--- client/src/constants/Config.js
+++
@@ -1,8 +0,0 @@
-export const TRANSACTION_STATUS = {
-    FAILURE: 0,
-    SUCCESS: 1,
-}
-
-export const SERVER = {
-    baseURL: process.env.REACT_APP_CORE_API || "https://backend-temp-uit.herokuapp.com",
-}
